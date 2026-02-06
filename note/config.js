df0cf021619d6a13b25cd90d518815de60d90f08
keicha2025 <wj209ing@gmail.com>
2026-02-06 19:28:20 +0800

    Update note.png

--- note/config.js
+++ note/config.js
@@ -1,3 +1,3 @@
 // configuration file
 // Replace with your deployed Google Apps Script Web App URL
-const GAS_API_URL = "https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec";
+const GAS_API_URL = "https://script.google.com/macros/s/AKfycbwSiWPN1AYgLh20ExGUXMSldS8s2f6P8JJktvq7Ckk9b3-Own2ZnC6-ZMzhLbvGzNeF/exec";
