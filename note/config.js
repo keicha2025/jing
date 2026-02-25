d0250d0f0a77a2408e83d53cd73968c682cbc6f4
keicha2025 <wj209ing@gmail.com>
2026-02-25 21:38:30 +0800

    style: update authDomain to web.app for cleaner login UI

--- note/config.js
+++ note/config.js
@@ -4,7 +4,7 @@ const GAS_API_URL = "https://script.google.com/macros/s/AKfycbwSiWPN1AYgLh20ExGU
 
 const firebaseConfig = {
     apiKey: "AIzaSyDFNKdGzAHeULoTfWOYatswKQmZ132FYVA",
-    authDomain: "gen-lang-client-0428297574.firebaseapp.com",
+    authDomain: "gen-lang-client-0428297574.web.app",
     projectId: "gen-lang-client-0428297574",
     storageBucket: "gen-lang-client-0428297574.firebasestorage.app",
     messagingSenderId: "1082284355568",
